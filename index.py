a729665d11b0b0b217835a6393d2318bc18599d2
Fatima Munir <81692316+fatima142munir@users.noreply.github.com>
2024-06-12 16:31:29 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,18 @@
+#area of triangle
+b = 10
+h = 7
+f = 0.5
+areaOfTriangle = f*b*h 
+print(areaOfTriangle)
+
+#area of circle
+s1 = 8
+s2 = 8
+areaOfSquare = s1*s2
+print(areaOfSquare)
+
+#area of circle
+pie = 3.14
+radius = 8
+areaOfCircle = pie*radius*radius
+print(areaOfCircle)  
